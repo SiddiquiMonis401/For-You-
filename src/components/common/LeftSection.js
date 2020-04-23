b2a1678b77290376d7cebf3ec20da4e051265a35
zubair <zubairkhanzada7@gmail.com>
2020-04-23 23:57:42 +0500

    Made ui responsive

--- src/components/common/LeftSection.js
+++ src/components/common/LeftSection.js
@@ -9,7 +9,7 @@ import './styles/left-section.scss';
 // TODO: Add imageSrc support, so background image update as per demand.
 function LeftSection({ welcomeNote, title, description, children }) {
 	return (
-		<div className="w-50">
+		<div className="w-50 hide-mobile-view">
 			<div className="bg-cover pl-5 w-100 d-flex flex-column justify-content-center">
 				<h4 className="mb-2 text-uppercase">{welcomeNote}</h4>
 				<h1 className="mb-5 pb-4 p-heading position-relative text-uppercase">{title}</h1>
--- src/components/common/RightSection.js
+++ src/components/common/RightSection.js
@@ -11,7 +11,7 @@ import './styles/right-section.scss';
 
 function RightSection({ withLogo, title, children }) {
 	return (
-		<div className="w-50 vh-100 d-flex flex-column justify-content-center align-items-center">
+		<div className="w-50 full-mobile-view vh-100 d-flex flex-column justify-content-center align-items-center">
 			<div className="text-center right-section">
 				{ withLogo && <img src={logo} alt="" width="90px"/> }
 				<h1 className="mb-5 mt-4 pt-2 pb-4 position-relative s-heading">{title}</h1>
