ee6bd23e35af2bb0f067045233564e3ee08ddfa3
zubair <zubairkhanzada7@gmail.com>
2020-05-05 00:18:49 +0500

    Implemented user details screen

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,5 +1,5 @@
 import React, { memo } from 'react';
-import { Container, Row, Col, Button, Table } from 'react-bootstrap';
+import { Container, Row, Col, Button, Table, Form } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 // styles
@@ -11,6 +11,12 @@ import user from 'assets/images/user.svg';
 import message from 'assets/images/message.svg';
 import logout from 'assets/images/logout.svg';
 import profile from 'assets/images/user-profile.png';
+import cross from 'assets/images/cross-sidebar.svg';
+import edit from 'assets/images/edit-icon.svg';
+import share from 'assets/images/share-icon.svg';
+import trash from 'assets/images/trash-icon.svg';
+import phone from 'assets/images/phone.svg';
+import envelope from 'assets/images/envelope.svg';
 
 function Dashboard() {
   return (
@@ -83,7 +89,9 @@ function Dashboard() {
                     <span className="item-container">Sharwani</span>
                   </td>
                   <td>4 April 2020</td>
-                  <td>cell</td>
+                  <td>
+                    <a href="#test">cell</a>
+                  </td>
                 </tr>
                 <tr>
                   <td>
@@ -170,6 +178,117 @@ function Dashboard() {
           </div>
         </Col>
       </Row>
+      <div className="user-overlay">
+        <div className="user-list">
+          <div className="clear-wrapper d-flex justify-content-end">
+            <a href="#test">
+              <img src={cross} alt="Cross Icon"/>
+            </a>
+          </div>
+          {/* // */}
+          <div className="user-detail d-flex justify-content-between align-items-center">
+            <div className="d-flex">
+              <div className="mr-3">
+                <img src={profile} alt="" width="60" height="60" />
+              </div>
+              <div className="d-flex flex-column justify-content-center">
+                <h4 className="mb-0">Name Surname</h4>
+                <span>@johndoe</span>
+              </div>
+            </div>
+            <div>
+              <a href="#test"><img src={edit} alt="Edit Icon"/></a>
+              <a href="#test"><img src={share} alt="Share Icon"/></a>
+              <a href="#test"><img src={trash} alt="Trash Icon"/></a>
+            </div>
+          </div>
+          {/* // */}
+          <div className="contact-info">
+            <h3>Contact info:</h3>
+            <div className="d-flex">
+              <img src={phone} alt="Phone Icon"/>
+              <span>+92 345 67890</span>
+            </div>
+            <div className="d-flex">
+              <img src={envelope} alt="Envelope Icon"/>
+              <span>user@gmail.com</span>
+            </div>
+          </div>
+          {/* // */}
+          <div className="clothe-detail mb-5">
+            <Form.Group className="mb-5" controlId="exampleForm.ControlSelect1">
+              <Form.Control as="select">
+                <option>Sharwani</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+                <option>5</option>
+              </Form.Control>
+            </Form.Group>
+            <h3>Clothe Detail:</h3>
+            <div className="d-flex">
+              <div className="details">
+                <h5>Clothe No:</h5>
+                <div className="d-flex align-items-center">D29472bs3</div>
+              </div>
+              <div className="details">
+                <h5>Clothe Color:</h5>
+                <div className="d-flex align-items-center">
+                  Navy Blue
+                  <span className="select-color" />
+                </div>
+              </div>
+              <div className="details">
+                <h5>Clothe Size:</h5>
+                <div className="d-flex align-items-center">4.5 Metro</div>
+              </div>
+            </div>
+          </div>
+          {/* // */}
+          <div className="clothe-measurement">
+            <h3>Clothe Measurement:</h3>
+            <div className="d-flex">
+              <div className="left-container">
+                <div className="d-flex justify-content-between">
+                  Length
+                  <span>39</span>
+                </div>
+                <div className="d-flex justify-content-between">
+                  Waist
+                  <span>37</span>
+                </div>
+                <div className="d-flex justify-content-between">
+                  Bottem
+                  <span>19</span>
+                </div>
+                <div className="d-flex justify-content-between">
+                  Knee
+                  <span>00</span>
+                </div>
+                <div className="d-flex justify-content-between">
+                  Thigh
+                  <span>29</span>
+                </div>
+                <div className="d-flex justify-content-between">
+                  Shoulder
+                  <span>19</span>
+                </div>
+              </div>
+              <div className="right-container">
+                <div className="d-flex">
+                  Long Belt
+                </div>
+                <div className="d-flex">
+                  2 Side Pocket
+                </div>
+                <div className="d-flex">
+                  Grip
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </Container>
   );
 }
