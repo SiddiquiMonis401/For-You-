38c353d7876b4cb6318c903c7ed619e7379733ab
umair-khanzada <umairkz52@gmail.com>
2020-05-05 22:37:17 +0500

    added font-awesome spinner icon.

--- src/fontawesomeLibrary.js
+++ src/fontawesomeLibrary.js
@@ -1,4 +1,9 @@
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fab } from '@fortawesome/free-brands-svg-icons';
+import { faSpinner, faCircleNotch } from '@fortawesome/free-solid-svg-icons';
 
-library.add(fab);
+library.add(
+  fab,
+  faSpinner,
+  faCircleNotch
+);
