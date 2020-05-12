813be9748f3d57342df3952ac8eeec5ad3f276a9
umair-khanzada <umairkz52@gmail.com>
2020-05-12 22:47:59 +0500

    updated dashboard component and added add-template route.

--- src/routes/PrivateRoutes.js
+++ src/routes/PrivateRoutes.js
@@ -1,22 +1,33 @@
 import React  from 'react';
-import { Route, Switch, Redirect } from 'react-router-dom';
+import {Route, Switch, Redirect} from 'react-router-dom';
+import { Container, Row, Col } from 'react-bootstrap';
 
 // Props
 import { PrivateRoutesProps } from 'props';
 
 // Components
-import { Dashboard } from 'components';
-import { TopBar } from 'components/common';
+import { Dashboard, AddTemplate } from 'components';
+import { TopBar, Sidebar } from 'components/common';
 
 const PrivateRoutes = (props) => {
   const { match: { path }} = props;
   return true ? (
     <>
       <TopBar />
-      <Switch>
-        <Route path={`${path}dashboard`} component={Dashboard} />
-        <Route path={`${path}`} component={Dashboard} />
-      </Switch>
+      <Container fluid>
+        <Row>
+          <Col md="3" lg="2" className="pl-0">
+            <Sidebar />
+          </Col>
+          <Col md="9" lg="10" className="p-0">
+            <Switch>
+              <Route path={`${path}/dashboard`} component={Dashboard} />
+              <Route path={`${path}/add-template`} component={AddTemplate} />
+              <Route path={`${path}`} component={Dashboard} />
+            </Switch>
+          </Col>
+        </Row>
+      </Container>
     </>
   ) : (
     <Redirect to="/" />
