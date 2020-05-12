fb3265d5517d99e50b34d534c1ad9df84c9c5006
umair-khanzada <umairkz52@gmail.com>
2020-05-12 22:48:35 +0500

    updated validation

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -1,10 +1,24 @@
 import * as Yup from 'yup';
 
+// Common
 const emailValidation = {
   email: Yup
     .string()
     .email('Invalid email address')
     .required('Email address is required'),
+};
+
+const nameValidation = {
+  name: Yup
+    .string()
+    .min(3, 'Name must be at least 3 characters long')
+    .required('Name is required'),
+};
+
+const desValidation = {
+  description: Yup
+    .string()
+    .max(1000, 'Description must be less than or equal to 1000 characters')
 };
 
 const passwordValidation = {
@@ -24,11 +38,22 @@ const confirmPasswordValidation = {
     .oneOf([Yup.ref('password')], 'Password must match'),
 };
 
+const templateFields = {
+  fields: Yup.array()
+    .of(
+      Yup.object().shape({
+        label: Yup.string()
+          .min(4, 'too short')
+          .required('Label is required'),
+      })
+    )
+    .required('Must have fields')
+    .min(3, 'Minimum of 3 fields'),
+};
+
+// Forms
 const signupValidation = Yup.object({
-  name: Yup
-    .string()
-    .min(3, 'Name must be at least 3 characters long')
-    .required('Name is required'),
+  ...nameValidation,
   ...emailValidation,
   ...confirmPasswordValidation
 });
@@ -42,8 +67,16 @@ const forgotPasswordValidation = Yup.object({
   ...emailValidation,
 });
 
+
+const addTemplateValidation = Yup.object({
+  ...nameValidation,
+  ...desValidation,
+  ...templateFields,
+});
+
 export default {
   signupValidation,
   loginValidation,
   forgotPasswordValidation,
+  addTemplateValidation,
 };
